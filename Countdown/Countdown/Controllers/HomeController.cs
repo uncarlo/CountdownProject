eff622d8d20a2ab77396818cabdc22d9424ccc07
Ben Stiefel <bstiefel@kpmg.com>
2015-02-14 17:51:58 -0800

    using service to display items

--- Countdown/Countdown/Controllers/HomeController.cs
+++ Countdown/Countdown/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CountdownService;
 
 namespace Countdown.Controllers
 {
@@ -10,7 +11,8 @@ namespace Countdown.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var movieList = new CountdownService.Controllers.MovieInfoController().Get();
+            return View(movieList.ToList());
         }
 
         public ActionResult About()
--- Countdown/Countdown/Scripts/main.js
+++ Countdown/Countdown/Scripts/main.js
@@ -6,15 +6,6 @@
             var today = new Date().setHours(0);
             var countdown = Math.round((date.getTime() - today) / oneDay);
             $(item).siblings('.days-until-release').text(countdown);
-            if (countdown < 0) {
-                $(item).siblings('.status').text('Released');
-            } else if (countdown < 366) {
-                $(item).siblings('.status').text('Post-production');
-            } else if (countdown < 732) {
-                $(item).siblings('.status').text('Pre-production');
-            } else {
-                $(item).siblings('.status').text('In Development');
-            }
         } catch(ex) {
             $(item).siblings('.days-until-release').text('TBA');
         }
