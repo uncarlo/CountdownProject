6510dbbd9457806496099a20f4c0a8fba9c5d676
Ben Stiefel <bstiefel@kpmg.com>
2015-02-14 16:26:23 -0800

    Base solution & front end project

---
+++ Countdown/Countdown/Scripts/main.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+    _.each($('.film .release-date'), function (item) {
+        try {
+            var oneDay = 1000 * 60 * 60 * 24;
+            var date = new Date($(item).text());
+            var today = new Date().setHours(0);
+            var countdown = Math.round((date.getTime() - today) / oneDay);
+            $(item).siblings('.days-until-release').text(countdown);
+            if (countdown < 0) {
+                $(item).siblings('.status').text('Released');
+            } else if (countdown < 366) {
+                $(item).siblings('.status').text('Post-production');
+            } else if (countdown < 732) {
+                $(item).siblings('.status').text('Pre-production');
+            } else {
+                $(item).siblings('.status').text('In Development');
+            }
+        } catch(ex) {
+            $(item).siblings('.days-until-release').text('TBA');
+        }
+    });
+});
---
+++ Countdown/Countdown/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Countdown.Startup))]
+namespace Countdown
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
